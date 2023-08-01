01f730223149714c818a786a5609299710fc9628
Neeraj Kalani <neerajkalani22@gmail.com>
2023-08-01 20:06:49 +0530

    initial

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const dbConfig = require('./config/dbConfig');
+const path = require('path');
+app.use(express.json());
+app.use(cors());
+const adminRoute = require('./Routes/adminRoutes');
+const userRoute = require('./Routes/userRoutes');
+const doctorsRoute = require('./Routes/doctorsRoutes');
+
+
+app.use('/api/user',userRoute);
+app.use('/api/admin',adminRoute);
+app.use('/api/doctor/',doctorsRoute);
+
+
+app.listen(port , () => {
+    console.log(`listning on port ${port}`);
+});
